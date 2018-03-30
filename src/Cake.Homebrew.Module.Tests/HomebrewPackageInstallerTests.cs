4d7002cd3a72f794489a696d0431292e4ee0e2fa
RLittlesII <rodney.littles@gmail.com>
2018-03-29 23:47:19 -0500

    Added PackageInstaller unit tests

---
+++ src/Cake.Homebrew.Module.Tests/HomebrewPackageInstallerTests.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Cake.Core.Packaging;
+using NSubstitute;
+using Xunit;
+
+namespace Cake.Homebrew.Module.Tests
+{
+    public sealed class HomebrewPackageInstallerTests
+    {
+        public sealed class TheConstructor
+        {
+            [Fact]
+            public void Should_Throw_If_Environment_Is_Null()
+            {
+                // Given
+                var fixture = new HomebrewPackageInstallerFixture();
+                fixture.Environment = null;
+
+                // When
+                var result = Record.Exception(() => fixture.CreateInstaller());
+
+                // Then
+                Assert.IsType<ArgumentNullException>(result);
+                Assert.Equal("environment", ((ArgumentNullException) result).ParamName);
+            }
+
+            [Fact]
+            public void Should_Throw_If_Content_Resolver_Is_Null()
+            {
+                // Given
+                var fixture = new HomebrewPackageInstallerFixture();
+                fixture.ContentResolver = null;
+
+                // When
+                var result = Record.Exception(() => fixture.CreateInstaller());
+
+                // Then
+                Assert.IsType<ArgumentNullException>(result);
+                Assert.Equal("contentResolver", ((ArgumentNullException) result).ParamName);;
+            }
+
+            [Fact]
+            public void Should_Throw_If_Log_Is_Null()
+            {
+                // Given
+                var fixture = new HomebrewPackageInstallerFixture();
+                fixture.Log = null;
+
+                // When
+                var result = Record.Exception(() => fixture.CreateInstaller());
+
+                // Then
+                Assert.IsType<ArgumentNullException>(result);
+                Assert.Equal("log", ((ArgumentNullException)result).ParamName); ;
+            }
+        }
+
+        public sealed class TheCanInstallMethod
+        {
+            private string HOMEBREW_CONFIGKEY = "Homebrew_Source";
+
+            [Fact]
+            public void Should_Throw_If_URI_Is_Null()
+            {
+                // Given
+                var fixture = new HomebrewPackageInstallerFixture();
+                fixture.Package = null;
+
+                // When
+                var result = Record.Exception(() => fixture.CanInstall());
+
+                // Then
+                Assert.IsType<ArgumentNullException>(result);
+                Assert.Equal("package", ((ArgumentNullException)result).ParamName);
+            }
+            [Fact]
+            public void Should_Be_Able_To_Install_If_Scheme_Is_Correct()
+            {
+                // Given
+                var fixture = new HomebrewPackageInstallerFixture();
+                fixture.Package = new PackageReference("homebrew:?package=fastlane");
+
+                // When
+                var result = fixture.CanInstall();
+
+                // Then
+                Assert.True(result);
+            }
+
+            [Fact]
+            public void Should_Not_Be_Able_To_Install_If_Scheme_Is_Incorrect()
+            {
+                // Given
+                var fixture = new HomebrewPackageInstallerFixture();
+                fixture.Package = new PackageReference("ruby:?package=fastlane");
+
+                // When
+                var result = fixture.CanInstall();
+
+                // Then
+                Assert.False(result);
+            }
+
+            [Fact]
+            public void Should_Ignore_Custom_Source_If_AbsoluteUri_Is_Used()
+            {
+                var fixture = new HomebrewPackageInstallerFixture();
+                fixture.Package = new PackageReference("homebrew:http://absolute/?package=windirstat");
+
+                // When
+                var result = Record.Exception(() => fixture.Install());
+
+                // Then
+                Assert.Null(result);
+                fixture.Config.DidNotReceive().GetValue(HOMEBREW_CONFIGKEY);
+            }
+        }
+    }
+}
--- src/Cake.Homebrew.Module/HomebrewPackageInstaller.cs
+++ src/Cake.Homebrew.Module/HomebrewPackageInstaller.cs
@@ -13,40 +13,79 @@ namespace Cake.Homebrew.Module
 {
     public sealed class HomebrewPackageInstaller : IPackageInstaller
     {
+        private readonly IProcessRunner _processRunner;
+        private readonly ICakeLog _log;
+        private readonly IHomebrewContentResolver _contentResolver;
+        private readonly ICakeConfiguration _configuration;
+        private readonly ICakeEnvironment _environment;
+
         public HomebrewPackageInstaller(ICakeEnvironment environment,
             IProcessRunner processRunner,
             ICakeLog log,
             IHomebrewContentResolver contentResolver,
             ICakeConfiguration configuration)
         {
-            if (environment == null)
-            {
-                throw new ArgumentNullException(nameof(environment));
-            }
+            _processRunner = processRunner ?? throw new ArgumentNullException(nameof(processRunner));
+            _log = log ?? throw new ArgumentNullException(nameof(log));
+            _contentResolver = contentResolver ?? throw new ArgumentNullException(nameof(contentResolver));
+            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
 
-            if (processRunner == null)
-            {
-                throw new ArgumentNullException(nameof(processRunner));
-            }
-
-            if (log == null)
-            {
-                throw new ArgumentNullException(nameof(log));
-            }
-
-            if (contentResolver == null)
-            {
-                throw new ArgumentNullException(nameof(contentResolver));
-            }
-
-            if (configuration == null)
-            {
-                throw new ArgumentNullException(nameof(configuration));
-            }
+            _environment = environment ?? throw new ArgumentNullException(nameof(environment));
         }
 
-        public bool CanInstall(PackageReference package, PackageType type) { throw new NotImplementedException(); }
+        public bool CanInstall(PackageReference package, PackageType type)
+        {
+            if (package == null)
+            {
+                throw new ArgumentNullException(nameof(package));
+            }
 
-        public IReadOnlyCollection<IFile> Install(PackageReference package, PackageType type, DirectoryPath path) { throw new NotImplementedException(); }
+            return package.Scheme.Equals("homebrew", StringComparison.OrdinalIgnoreCase);
+        }
+
+        public IReadOnlyCollection<IFile> Install(PackageReference package, PackageType type, DirectoryPath path)
+        {
+            if (package == null)
+            {
+                throw new ArgumentNullException(nameof(package));
+            }
+
+            if (path == null)
+            {
+                throw new ArgumentNullException(nameof(path));
+            }
+
+            path = path.MakeAbsolute(_environment);
+
+            _log.Debug("Installing Homebrew package {0}...", package.Package);
+
+            var process = _processRunner.Start("brew",
+                new ProcessSettings
+                {
+                    Arguments = GetArguments(package, path, _configuration),
+                    RedirectStandardOutput = true,
+                    Silent = _log.Verbosity < Verbosity.Diagnostic
+                });
+
+            var result = _contentResolver.GetFiles(package, type);
+            if (result.Count != 0)
+            {
+                return result;
+            }
+
+            return result;
+        }
+
+        private ProcessArgumentBuilder GetArguments(PackageReference package,
+            DirectoryPath path,
+            ICakeConfiguration configuration)
+        {
+            var arguments = new ProcessArgumentBuilder();
+
+            arguments.Append("install");
+            arguments.AppendQuoted(package.Package);
+
+            return arguments;
+        }
     }
 }
